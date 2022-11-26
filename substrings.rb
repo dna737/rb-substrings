b71bfe57d9dcd0178f740477639627908aeecef2
dna737 <adnanhere27@gmail.com>
2022-11-26 13:20:16 +0530

    return correct hash and ignore .vscode

--- substrings.rb
+++ substrings.rb
@@ -1,14 +1,21 @@
 def substrings(word = "Hello", dictionary = ["Hello", "world"])
-    new_arr = Array.new(0, "")
+    new_arr = Array.new(0)
     dictionary.each do|element|
-        #if the `word` includes the element then add it.
+        #when the `word` includes the element then add it.
         if word.downcase.include?(element.downcase) then 
             new_arr.push(element)
         end
     end
-    #Modifying array for easier conversion into hash: create subarrays and go through each element and add the count as the second element for each subarray.
-    
+    #Create a new 2D array that stores the word and its count. This will be easy to convert to a hash.
+
+    #adding each word and its count to the new array:
+    final_arr = Array.new(0)
+    new_arr.each do|word|
+        final_arr.push(Array.new(0).push(word).push(new_arr.count(word)))
+    end
+    final_arr.to_h
 end
 
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
